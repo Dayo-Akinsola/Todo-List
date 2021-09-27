7d8606464bffdba3549ed3abfd7a2ccf0ef09fb0
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-09-27 19:56:27 +0100

    Imported pageEffects.js module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import { TodoListItem, Project} from './createProjects.js';
 import './style.css';
 
+import { TodoListItem, Project} from './createProjects.js';
+import { displayNewProjectForm, showSidebarProjectTasks } from './pageEffects.js';
+
+displayNewProjectForm();
+showSidebarProjectTasks();
