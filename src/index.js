1c5664b95aa504195f2faf988558f1f1d8c9f77f
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-09-28 17:44:20 +0100

    Imported displayNewTask function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import './style.css';
 
 import { Task, Project} from './createProjects.js';
-import { displayNewProjectForm, showSidebarProjectTasks } from './pageEffects.js';
+import { displayNewProjectForm, showSidebarProjectTasks, displayNewTaskForm } from './pageEffects.js';
 import {addNewProjectListener} from './domChanges.js';
 
 displayNewProjectForm();
 showSidebarProjectTasks();
 addNewProjectListener();
+displayNewTaskForm();
+
 
