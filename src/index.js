eb07c51988d192cfd13ba5220eb86df27ae315ae
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-09-29 22:51:29 +0100

    Imported checks for priority buttons

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import './style.css';
 
 import { Task, Project} from './createProjects.js';
-import { displayNewProjectForm, showSidebarProjectTasks, displayNewTaskForm } from './pageEffects.js';
-import {addNewProjectListener} from './domChanges.js';
+import { displayNewProjectForm, showSidebarProjectTasks, displayNewTaskForm, priorityButtonsChange } from './pageEffects.js';
+import {addNewProjectListener} from './projectDomChanges.js';
 
 displayNewProjectForm();
 showSidebarProjectTasks();
 addNewProjectListener();
 displayNewTaskForm();
+priorityButtonsChange();
 
 
