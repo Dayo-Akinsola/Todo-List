eb62d2ead92e20ddca543852e63b2c94a3dec87d
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-09-30 01:41:32 +0100

    Tasks can now be added for each project and are displayed in the right places on the main page and sidebar

--- src/taskDomChanges.js
+++ src/taskDomChanges.js
@@ -1,70 +1,45 @@
-import { DynamicEntryPlugin } from 'webpack';
-import { Project, Task, projectsObject } from './createProjects.js'
-import { priorityButtonCheck } from './pageEffects.js';
+import { Task, projectsObject } from './createProjects.js';
+import { createTaskMainDetails, createTaskButtons, createDetailsContainer } from '../helpers/taskDomHelpers.js';
 
 /*
     The function finds the project that the user wants to add
     a task to and adds it to that project.
 */
-const addNewTask = (task) => {
-    const projects = document.querySelectorAll('.project-details');
-    projects.forEach(project => {
-        if (Array.from(project.classList).includes('active')){
-            const taskList = document.querySelector('todo-list-items');
-            const taskListItem = document.createElement('div');
-            taskListItem.classList.add('todo-list-item');
-            taskListItem.setAttribute('data-taskId', task.id);
+const addTaskMainPage = (task, project) => {
+    const taskList = project.querySelector('.todo-list-items');
 
-            const summaryContainer = document.createElement('div');
-            summaryContainer.classList.add('summary-container');
+    const taskListItem = document.createElement('div');
+    taskListItem.classList.add('todo-list-item');
+    taskListItem.setAttribute('data-taskId', task.id);
 
-            const radioTitleContainer = document.createElement('div');
-            radioTitleContainer.classList.add('radio-title-container');
-            const taskRadio = document.createElement('input');
-            taskRadio.classList.add('todo-check'); taskRadio.type = 'radio';
-            const taskTitle = document.createElement('span');
-            taskTitle.classList.add('todo-title'); taskTitle.textContent = task.title;
-            radioTitleContainer.appendChild(taskRadio); radioTitleContainer.appendChild(taskTitle);
+    const summaryContainer = document.createElement('div');
+    summaryContainer.classList.add('summary-container');
 
-            summaryContainer.appendChild(radioTitleContainer);
+    const mainDetailsContainer = createTaskMainDetails(task);
+    summaryContainer.appendChild(mainDetailsContainer);
 
-            const taskDate = document.createElement('span');
-            taskDate.classList.add('todo-date'); taskDate.textContent = task.dueDate;
-            summaryContainer.appendChild(taskDate);
+    const taskDate = document.createElement('span');
+    taskDate.classList.add('todo-date'); taskDate.textContent = task.dueDate;
+    summaryContainer.appendChild(taskDate);
 
-            const editTaskButtons = document.createElement('div');
-            editTaskButtons.classList.add('edit-task-buttons');
+    const editTaskButtons = document.createElement('div');
+    editTaskButtons.classList.add('edit-task-buttons');
+    const buttonArray = createTaskButtons();
+    buttonArray.forEach(button => editTaskButtons.appendChild(button)); 
+    summaryContainer.appendChild(editTaskButtons);
 
-            const detailsButton = document.createElement('button');
-            const editButton = document.createElement('button');
-            const deleteButton = document.createElement('button');
-            const buttonArray = [ detailsButton, editButton, deleteButton];
-            buttonArray.forEach(button => button.type = 'button');
-            detailsButton.classList.add('todo-details'); detailsButton.textContent = 'Details';
-            editButton.classList.add('todo-edit'); editButton.textContent = 'Edit';
-            deleteButton.classList.add('todo-delete'); deleteButton.textContent = 'Delete';
+    taskList.appendChild(summaryContainer);
 
-            buttonArray.forEach(button => editTaskButtons.appendChild(button));
-            summaryContainer.appendChild(editTaskButtons);
-            taskList.appendChild(summaryContainer);
-
-            const detailsContainer = document.createElement('div');
-            const todoNotes = document.createElement('p');
-            detailsContainer.classList.add('details-container'); todoNotes.classList.add('todo-notes');
-            detailsContainer.appendChild(todoNotes);
-            taskList.appendChild(detailsContainer);
-
-            return project;
-        }
-    })
+    const detailsContainer = createDetailsContainer(task);
+    taskList.appendChild(detailsContainer);
 }
 
 // Matches projectid from main page and sidebar so the task is added to the right project on the sidebar
-const addNewTaskToSidebar = (task, project) => {
+const addTaskSidebar = (task, project) => {
     const sidebarProjects = document.querySelectorAll('.sidebar-project');
     
     sidebarProjects.forEach(sidebarProject => {
-        if (sidebarProject.dataset.projectid = project.dataset.projectid){
+        if (sidebarProject.dataset.projectid === project.dataset.projectid){
             const projectTasks = sidebarProject.querySelector('.btn-toggle.show');
             
             const taskTitle = document.createElement('li');
@@ -79,17 +54,46 @@ const addNewTaskToSidebar = (task, project) => {
     })
 }
 
-const newTasklistener = (project) => {
+const newTasklistener = () => {
+    const projects = document.querySelectorAll('.project-details');
     const form = document.querySelector('#add-task-form');
     const formTitle = document.querySelector('#task-form-name');
     const formNotes = document.querySelector('#notes-form');
-    const formDate = document.querySelector('task-form-date');
-    const addTaskButton = document.querySelector('#submit-task-button');
+    const formDate = document.querySelector('#task-form-date');
     const cancelButton = document.querySelector('#cancel-task-button');
-    const priorityButtons = document.querySelectorAll('.priority-button');
+    const addTaskScreen = document.querySelector('.add-task-screen');
+
 
     form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const newTask 
+        projects.forEach(project => {
+            if (Array.from(project.classList).includes('active')){
+                event.preventDefault();
+                const projectInstance = projectsObject[project.dataset.projectid];
+                const priority = document.querySelector('.priority-button.clicked');
+                const task = new Task(formTitle.value, formNotes.value, formDate.value, priority.id)
+                projectInstance.addTask(task);
+                addTaskMainPage(task, project);
+                addTaskSidebar(task, project);
+        
+                // reset form form after submitting
+                formTitle.value = ''; formNotes.value = ''; formDate.value = '';
+                addTaskScreen.style.display = 'none';
+                project.classList.remove('active');
+            }
+        })
+    })
+
+    cancelButton.addEventListener('click', () => {
+        projects.forEach(project => {
+            if (Array.from(project.classList).includes('active')){
+                formTitle.value = ''; formNotes.value = ''; formDate.value = '';
+                addTaskScreen.style.display = 'none';
+                project.classList.remove('active');
+            }
+        })
     })
 }
+ 
+
+
+export { newTasklistener };
