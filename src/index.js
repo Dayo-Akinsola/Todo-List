37dc5c1eedb42dcc451406e9344dc8f4d3e9bdda
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-09-30 01:39:27 +0100

    newTaskListener imported

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import './style.css';
 import { Task, Project} from './createProjects.js';
 import { displayNewProjectForm, showSidebarProjectTasks, displayNewTaskForm, priorityButtonsChange } from './pageEffects.js';
 import {addNewProjectListener} from './projectDomChanges.js';
+import { newTasklistener } from './taskDomChanges.js';
 
 displayNewProjectForm();
 showSidebarProjectTasks();
 addNewProjectListener();
 displayNewTaskForm();
 priorityButtonsChange();
-
+newTasklistener();
 
