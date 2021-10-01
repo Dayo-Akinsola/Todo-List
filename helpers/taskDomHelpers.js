1222aee33883cf5b5890da51691ba7e0585ea94f
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-10-01 01:16:07 +0100

    detailsContainer now includes date and priority

--- helpers/taskDomHelpers.js
+++ helpers/taskDomHelpers.js
@@ -16,20 +16,35 @@ const createTaskButtons = () => {
     const deleteButton = document.createElement('button');
     const buttonArray = [ detailsButton, editButton, deleteButton];
     buttonArray.forEach(button => button.type = 'button');
-    detailsButton.classList.add('todo-details'); detailsButton.textContent = 'Details';
-    editButton.classList.add('todo-edit'); editButton.textContent = 'Edit';
-    deleteButton.classList.add('todo-delete'); deleteButton.textContent = 'Delete';
+    detailsButton.classList.add('details-button'); detailsButton.textContent = 'Details';
+    editButton.classList.add('edit-button'); editButton.textContent = 'Edit';
+    deleteButton.classList.add('delete-button'); deleteButton.textContent = 'Delete';
 
     return buttonArray;
 }
 
 const createDetailsContainer = (task) => {
     const detailsContainer = document.createElement('div');
-    const todoNotes = document.createElement('p');
     detailsContainer.classList.add('details-container'); 
-    todoNotes.classList.add('todo-notes');
+    detailsContainer.classList.add('hidden');
+
+    const todoNotes = document.createElement('p');
+    todoNotes.id = 'todo-notes'; todoNotes.classList.add('details');
     todoNotes.textContent = task.notes;
     detailsContainer.appendChild(todoNotes);
+    
+    const taskDate = document.createElement('span');
+    taskDate.id = 'todo-date'; taskDate.classList.add('details');
+    taskDate.textContent = 'Due: ' + task.dueDate; 
+    detailsContainer.appendChild(taskDate);
+
+    const taskPriority = document.createElement('span');
+    taskPriority.id = 'todo-priority'; taskPriority.classList.add('details');
+    taskPriority.textContent = 'Priority: ' + task.priority;
+    if (task.priority === 'low') taskPriority.style.color = '#84f14e';
+    else if (task.priority === 'normal') taskPriority.style.color = '#f1bb4e';
+    else taskPriority.style.color = '#f14e4e';
+    detailsContainer.appendChild(taskPriority);
 
     return detailsContainer;
 }
