a284bee4555ec293a1f45b5cf80ae2c4357cdb70
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-10-01 01:17:19 +0100

    Included functions to hide/show task details

--- src/pageEffects.js
+++ src/pageEffects.js
@@ -1,5 +1,4 @@
 // This module creates functions that dynamically change css properties 
-
 const displayNewProjectForm = () => {
     const addProjectButton = document.querySelector('.add-project-container');
     const addProjectForm = document.querySelector('.add-project-screen');
@@ -55,4 +54,50 @@ const priorityButtonsChange = () => {
     })
 }
 
-export { displayNewProjectForm, showSidebarProjectTasks, displayNewTaskForm, priorityButtonsChange }
+const increaseSidebarHeight = () => {
+    const sidebar = document.querySelector('.sidebar');
+    const mainPageHeight = document.querySelector('.todos-container').clientHeight;
+
+    sidebar.style.minHeight = mainPageHeight + 50 + "px";
+}
+
+const showDetails = (event) => {
+    const taskItems = document.querySelectorAll('.todo-list-item');
+    let taskItem
+    taskItems.forEach(task => {
+        if (task.querySelector('.details-button') === event.target){
+            taskItem = task;
+        }
+    })
+    const detailsContainer = taskItem.querySelector('.details-container');
+    if (Array.from(detailsContainer.classList).includes('hidden')){
+        detailsContainer.classList.remove('hidden');
+    }
+    else detailsContainer.classList.add('hidden');
+    increaseSidebarHeight();
+}
+
+/*
+    Since the listener loops through all buttons again when a new
+    task is added the removeEventListener is included so the buttons
+    don't have multiple click events attached to them.
+*/
+const showDetailsListener = () => {
+    const detailsButtons = document.querySelectorAll('.details-button');
+
+    detailsButtons.forEach(button => {
+        button.removeEventListener('click', showDetails)
+        button.addEventListener('click', showDetails)
+    })
+}
+
+
+
+export { 
+    displayNewProjectForm, 
+    showSidebarProjectTasks, 
+    displayNewTaskForm, 
+    priorityButtonsChange, 
+    increaseSidebarHeight, 
+    showDetailsListener,
+}
