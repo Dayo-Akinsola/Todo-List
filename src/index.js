465f12a6132e4f2f24686c2982ca9e8df5d43643
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-10-01 01:16:31 +0100

    Imported showDetailsListener

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 
 import { Task, Project} from './createProjects.js';
-import { displayNewProjectForm, showSidebarProjectTasks, displayNewTaskForm, priorityButtonsChange } from './pageEffects.js';
+import { displayNewProjectForm, showSidebarProjectTasks, displayNewTaskForm, priorityButtonsChange, showDetailsListener } from './pageEffects.js';
 import {addNewProjectListener} from './projectDomChanges.js';
 import { newTasklistener } from './taskDomChanges.js';
 
@@ -11,4 +11,4 @@ addNewProjectListener();
 displayNewTaskForm();
 priorityButtonsChange();
 newTasklistener();
-
+showDetailsListener();
